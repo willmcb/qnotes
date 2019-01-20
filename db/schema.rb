50e275e6455b8edac2aa9f0c7b5908f513c2c384
will mcbrien <williamjamesmcbrien@gmail.com>
2019-01-20 21:29:16 +0000

    generate collections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_151458) do
+ActiveRecord::Schema.define(version: 2019_01_20_212709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
