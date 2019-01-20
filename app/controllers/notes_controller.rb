cce13570bb8416bc7ab385ec19dde6c6f0c5bc89
will mcbrien <williamjamesmcbrien@gmail.com>
2019-01-20 19:37:03 +0000

    Add ability to create notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,35 @@
+class NotesController < ApplicationController
+  def new
+    @note = Note.new
+  end
+
+  def create
+    if current_user
+      note = current_user.notes.new(note_params)
+      if note.save!
+         puts "Note has been saved"
+      else
+        flash[:notice] = note.errors.full_messages.join(", ")
+      end
+      redirect_to show_note_path(id: note.id)
+    else
+      flash[:notice] =  "Please login to post a note."
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @note = Note.find(show_note_params)
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
+
+  def show_note_params
+    params.require(:id)
+  end
+end
+
