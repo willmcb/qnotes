6b2c0ae2cfee25a4c4a6708ac1dfa6817b18fa2b
will mcbrien <williamjamesmcbrien@gmail.com>
2019-01-20 12:30:43 +0000

    remove deprecated feature files

--- spec/features/user_can_visit_homepage_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Homepage is available" do
-  it "allows user to see the home page at the root path" do
-    visit "/"
-    expect(page).to have_content("Qnotes")
-  end
-end
