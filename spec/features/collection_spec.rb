37567c9c94a337d8eacea4e53ef61e7321921f86
willmcb <williamjamesmcbrien@gmail.com>
2019-01-24 23:22:29 +0000

    Add further test

--- spec/features/collection_spec.rb
+++ spec/features/collection_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Notes workflow" do
 
   let(:collection_name) { "ruby" }
+  let(:another_collection_name) { "python-2.7" }
 
   before :each do
     @test_user = create(:user)
@@ -11,9 +12,18 @@ RSpec.describe "Notes workflow" do
   it "allows a user to create aan view collection" do
     login
     visit 'collections/new'
-    fill_in 'Name', with: 'ruby'
+    fill_in 'Name', with: collection_name
     click_button 'Add collection'
     expect(page).to have_content(collection_name)
+  end
+
+  it 'allows a new collection to be selected when the user tries to add a new note' do
+    login
+    visit 'collections/new'
+    fill_in 'Name', with: another_collection_name
+    click_button 'Add collection'
+    visit 'notes/new'
+    expect(page).to have_content(another_collection_name)
   end
 
   def login
